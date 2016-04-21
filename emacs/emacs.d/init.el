64af847a22dbe7e51783972b05a4d7bc8ff5a124
Andrej Kuročenko <kurochenko@Andrejs-MBP.lan>
2016-04-21 14:17:40 +0200

    Init.

---
+++ emacs/emacs.d/init.el
@@ -0,0 +1,5 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade.ferrier.me.uk/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(package-initialize)
